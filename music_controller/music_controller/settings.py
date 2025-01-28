2a5eff73f80bfad2db420d3e8278f798a0aa82b9
kdev307 <127479436+kdev307@users.noreply.github.com>
2025-01-28 16:35:12 +0530

    Frontend Setup

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000", 
+# ]
